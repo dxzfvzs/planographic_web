4a8fc6a2c9edae0f82006e8d2aee516f382648dd
Kristýna Fuchsová <kristynafuchsova@seznam.cz>
2026-02-27 22:07:02 +0100

    feat: reflect drop of eng questions from foreign language in its intro

--- src/utils/subjects.tsx
+++ src/utils/subjects.tsx
@@ -24,7 +24,7 @@ export const subjects = {
     cz: "Angličtina",
     color: "#61981e",
     url: "english",
-    intro: "Angličtina pokryje jak běžné maturitní otázky z každodenního života, tak i ty odborné z pohledu polygrafie.",
+    intro: "Angličtina pokryje jak běžné maturitní otázky z každodenního života i celého světa.",
   },
   Czech: {
     cz: "Čeština",
