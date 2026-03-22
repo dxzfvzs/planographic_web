2a0af382d28621706a3c45fcb091d8c0ec68c442
dxzfvzs <56274079+dxzfvzs@users.noreply.github.com>
2026-03-22 02:40:17 +0100

    feat: update footer links (#3)
    * feat: update footer links
    
    * fix: fix codereview comments

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,7 +5,22 @@ export default function Footer() {
   return (
     <footer>
       <span>
-          planographic@post.cz
+        <a href="https://github.com/dxzfvzs/planographic_web"
+           className="link"
+           target="_blank"
+           rel="noopener noreferrer"
+           aria-label="GitHub Repozitář (otevře se v novém okně)"
+        >
+          GitHub Repozitář
+        </a>
+        <a href="https://planographic.8u.cz/"
+           className="link"
+           target="_blank"
+           rel="noopener noreferrer"
+           aria-label="Původní stránka (otevře se v novém okně)"
+        >
+          Původní stránka
+        </a>
       </span>
     </footer>
   );
